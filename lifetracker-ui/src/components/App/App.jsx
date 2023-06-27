84a4d25154271ec1155e65affbea00af117f53aa
Thomás Kuhn <thomas.kuhn@thomask-ltm4wrw.internal.salesforce.com>
2023-06-27 15:37:40 -0700

    first ui setup

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
+import Navbar from "../Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "../LoginPage/LoginPage";
+
 
 function App() {
 
   return (
-    <div>
+    <div className='app'>
+
+      <Navbar/>
+
+
+      <BrowserRouter>
+         
+         <Routes>
+          {/* <Route path="/" element={<Landing />} /> */}
+          <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/register" element={<RegistrationPage />} /> */}
+        </Routes>
+      
+      
+      </BrowserRouter>
+
+      
 
     </div>
   )
---
+++ lifetracker-ui/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import "./LoginPage.css";
+
+export default function LoginPage() {
+  return (
+
+    <div class="css-9cjjy5">
+      <div class="chakra-stack css-15xvz01">
+        <span class="chakra-avatar css-11g7hql">
+          
+        </span>
+        <h2 class="chakra-heading css-3q8efk">Welcome</h2>
+        <div class="css-ebzegt">
+          <form>
+            <div class="chakra-stack css-1opnp10">
+              <div role="group" class="chakra-form-control css-1kxonj9">
+                <div class="chakra-input__group css-bx0blc" data-group="true">
+                  <div class="chakra-input__left-element css-1cw84h2">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      class="css-119zpey"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path>
+                    </svg>
+                  </div>
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    id="field-:r0:"
+                    required=""
+                    aria-required="true"
+                    class="chakra-input css-trvw4f"
+                    value=""
+
+                  ></input>
+                </div>
+              </div>
+              <div role="group" class="chakra-form-control css-1kxonj9">
+                <div class="chakra-input__group css-bx0blc" data-group="true">
+                  <div class="chakra-input__left-element css-17ke578">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 448 512"
+                      class="css-119zpey"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path>
+                    </svg>
+                  </div>
+                  <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    id="field-:r1:"
+                    required=""
+                    aria-required="true"
+                    class="chakra-input css-67vh0"
+                    value=""
+
+                  ></input>
+                  <div class="chakra-input__right-element css-1qww07b">
+                    <button type="button" class="chakra-button css-18zqh0c">
+                      Show
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <button type="submit" class="chakra-button css-4lvvxn">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="css-0">
+        New to us?{" "}
+        <a class="chakra-link css-c6nly4" href="/register">
+          Sign Up
+        </a>
+      </div>
+    </div>
+  );
+}
